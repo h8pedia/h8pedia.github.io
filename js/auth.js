e3199b45b372e9713964d02d0b94ac48eb7b7330
h8pedia <parth.social.dev@gmail.com>
2026-02-10 21:51:15 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -39,8 +39,8 @@ var Auth = (function() {
     if (!username || username.length < 3) {
       return { ok: false, error: "Username must be at least 3 characters." };
     }
-    if (!email.endsWith("@hackleyschool.org")) {
-      return { ok: false, error: "You must use a Hackley email (@hackleyschool.org)." };
+    if (!email.endsWith("@students.hackleyschool.org")) {
+      return { ok: false, error: "You must use a Hackley email (@students.hackleyschool.org)." };
     }
     if (!password || password.length < 6) {
       return { ok: false, error: "Password must be at least 6 characters." };
